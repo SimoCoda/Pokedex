3e6080a5dfad9d6db3ef09a8793e27b3a93452cc
Simone Codarin <109222820+SimoCoda@users.noreply.github.com>
2025-02-06 17:49:16 +0100

    Modificata App.vue per la fase responsive del titolo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import listPokemon from './components/listPokemon.vue'
 <template>
   <div class="container">
     <h1
-      class="text-7xl text-left pl-8 md:text-center md:pl-0 lg:text-center lg:pl-0 xl:text-center xl:pl-0 text-white font-bold pt-16"
+       class="text-5xl lg:text-6xl xl:text-7xl text-left pl-8 md:text-center md:pl-0 lg:text-center lg:pl-0 xl:text-center xl:pl-0 text-white font-bold pt-16"
     >
       Pokedex
     </h1>
